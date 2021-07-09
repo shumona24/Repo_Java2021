b8e9e556b9a907ae7e5bd85953717da36555afa5
Shovona <shovona@gmail.com>
2021-07-08 21:50:38 -0400

    Homework 1

---
+++ src/HomeWork1/Project1.java
@@ -0,0 +1,45 @@
+package HomeWork1;
+
+public class Project1 {
+
+  // main method
+  // My First HomeWork Java Project
+
+  public static void main(String[] args) {
+      // Q1: number of hours in a day (byte)
+  byte numberOfHoursInaDay =24;
+      System.out.println("\nnumberOfHoursInaDay:24 ");
+      // Q2.Max.no of days in an year
+  short maxNumberOfTheDayInaYear = 366;
+      System.out.println("\nmaxNumberOfTheDaysInaYear:366");
+      // Q3.Total number of employees in an organization
+  int totalNumberOfEmployeesInIbm = 50000;
+      System.out.println("\ntotalNumberOfEmployeesInIbm:50000");
+       //Q4.Population in a country
+  long numberOfPopulation = 2000000000L;
+      System.out.println("\nnumberOfPopulation : 2000000000L");
+       //Q5.Interest rate
+  float interestRate = 1.9f;
+      System.out.println("\\nCurrent interest rate:\" + interestRate");
+      //Q6. Balance in a bank account
+  long accountBalance = 50000000L;
+      System.out.println("\naccountBalance = 50000000L");
+
+
+       // Q7.Initials of your name
+  char firstInitial = 'S';
+  char lastInitial = 'M';
+      System.out.println("My initials: " +firstInitial + lastInitial);
+
+
+
+
+
+  }
+
+
+
+
+
+
+}
