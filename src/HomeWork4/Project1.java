6444cf4c1b9b7a9b4731d038c5a4d65071b66201
Shovona <shovona@gmail.com>
2021-08-01 23:37:05 -0400

    HomeWork4

---
+++ src/HomeWork4/Project1.java
@@ -0,0 +1,41 @@
+package HomeWork4;
+
+public class Project1 {
+
+
+
+    public static void main(String[] args) {
+
+        // using String HomeWork4
+
+        // Question - 1:
+
+        String threeWordSentence = "hApPy nEW yeAr";
+
+        System.out.println("Before code : " + threeWordSentence);
+
+        String threeWordSentence1 = "Happy New Year";
+
+        System.out.print("After code : " + threeWordSentence1);
+
+
+        //Question - 2: create the abbreviation  // HNY
+
+        char abbr1 = 'V';
+        char abbr2 = 'G';
+        char abbr3 = 'M';
+        System.out.println("Abbreviation for: " + abbr1 + abbr2 + abbr3);
+        // Abbreviation for 'very gOOD MorNING' is : VGM
+
+
+            // Question - 3:
+        /**
+         * print the number of words in the sentence.
+         */
+        String sentence_3 = "We all are here to learn programming and testing";
+        String numberOfWords = "9";
+        System.out.println("numberOfWords:" + numberOfWords);
+
+
+    }
+    }
