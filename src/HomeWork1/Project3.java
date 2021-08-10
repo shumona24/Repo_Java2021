82ab4fa04281295979f145009aec0018823860d8
Shovona <shovona@gmail.com>
2021-08-09 20:39:10 -0400

    src/MidtearmJavaExam/

--- src/HomeWork1/Project3.java
+++ src/HomeWork1/Project3.java
@@ -4,6 +4,5 @@ public class Project3 {
     public static void main(String[] args) {
 
 
-
     }
 }
---
+++ src/HomeWork4/ShortArrayExample.java
@@ -0,0 +1,21 @@
+package HomeWork4;
+
+public class ShortArrayExample {
+    public static void main(String[] args) {
+
+
+String threeWordSentence_2 = "very good morning";
+String abbr = "vgm";
+System.out.println("Abbreviation for 'threeWordsSentence_2'" + "' is : " + abbr);
+
+
+    }
+
+        }
+
+
+
+
+
+
+
