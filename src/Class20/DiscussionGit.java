1bccb1b927fad4deee53aa221483532a459dfdad
Shovona <shovona@gmail.com>
2021-09-12 09:10:20 -0400

    adding git commands

---
+++ src/Class20/DiscussionGit.java
@@ -0,0 +1,11 @@
+package Class20;
+
+public class DiscussionGit {
+    public static void main(String[] args) {
+
+        System.out.println("Discussing Git");
+
+
+
+    }
+}
--- src/com/company/Bank1.java
+++ src/com/company/Bank1.java
@@ -84,3 +84,4 @@ public class Bank1 {
         }
     }
 
+// please submit homework
